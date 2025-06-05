edf53db5c77dc963ba7250b66687b880e60fd8da
manulopoli <manu.lopes@gmail.com>
2025-06-05 14:44:04 +0000

    quase la

--- xadrez.c
+++ xadrez.c
@@ -7,26 +7,36 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int torre, bispo, rainha;
+    int i;
+    int direita, esquerda, cima;
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Mover o Bispo 5 casas para direira e para cima
+    while (i < 5) {
+        printf("Mova o Bispo para a direira\n"); //Para se mover em diagonal precisa andar 5 casas para a direita e 5 casas para cima
+        printf("Mova o Bispo para cima\n", i);
+        i++; //incremento
+    }
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Mover a Torre 5 casas para a direita
+    for (i = 0; i < 5; i++) //iniciação, condição, incremento
+    { 
+        printf("Mova a Torre para a direita\n");
+    }
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Mover a Rainha 8 casas para a esquerda
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    do {
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+        printf("Mova a Rainha para a esquerda\n");
+        i++; //incremento
+    }
+    
+    while (i == 8);
+    
 
     return 0;
 }
